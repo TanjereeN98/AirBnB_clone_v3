dd4b232adeb190076b890a54f97e8dd40d6ce9d1
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-11 00:07:34 +0300

    Adding last 4 tasks

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-"""
-app instantiation
-"""
+"""flask app entery point"""
+
+from os import getenv
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
-import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def close(e):
-    """ close the storage """
-    storage.close()
-
-
 @app.errorhandler(404)
-def page_not_found(error):
-    """handler for 404 errors"""
+def not_found(error):
+    """not found route"""
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+@app.teardown_appcontext
+def teardown(exception):
+    """teardown appcontext"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    """port and host"""
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+
     app.run(host=host, port=port, threaded=True)
